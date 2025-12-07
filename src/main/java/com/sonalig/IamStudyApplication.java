2da6608a5e1c89fe35e527e4bed9c4db402159ec
tangled-horizon <sonali.gadegaonkar@gmail.com>
2025-12-07 23:55:24 +0530

    New changes for spring boot

--- src/main/java/com/sonalig/IamStudyApplication.java
+++ src/main/java/com/sonalig/IamStudyApplication.java
@@ -1,4 +1,11 @@
 package com.sonalig;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class IamStudyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IamStudyApplication.class, args);
+    }
 }
